e6d20d249b9c0424773e919c09dcad21bd61ecf1
pratyu10 <mpratyusha0@gmail.com>
2021-06-17 12:42:17 +0530

    Second Commit

--- app/src/main/java/com/example/roomdatabaseexample/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabaseexample/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.roomdatabaseexample
 
+import WordRepository
+import WordViewModel
+import WordViewModelFactory
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var wordViewModel: WordViewModel
+//
+//    private val wordViewModel: WordViewModel by viewModels {
+//        WordViewModelFactory((application as WordsApplication).repository)
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val application: Application = getApplication()
+        val repository: WordRepository = (application as WordsApplication).repository
+        val factory = WordViewModelFactory(repository)
+        wordViewModel = ViewModelProvider(this, factory).get(WordViewModel::class.java)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.students_recycler_view)
+
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+
+        val editStudent: EditText = findViewById(R.id.edit_student)
+        val button: Button = findViewById(R.id.button_save)
+        button.setOnClickListener {
+            val name = editStudent.text.toString()
+            wordViewModel.insert(Word(name))
+        }
+
+        wordViewModel.allWords.observe(this, { list ->
+            adapter.submitList(list)
+        })
     }
+
 }
--- app/src/main/java/com/example/roomdatabaseexample/WordDatabase.kt
+++ app/src/main/java/com/example/roomdatabaseexample/WordDatabase.kt
@@ -1,8 +1,33 @@
 package com.example.roomdatabaseexample
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordDatabase: RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: WordDatabase? = null
+
+        fun getDatabase(context: Context): WordDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordDatabase::class.java,
+                    "word_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
 }
